f8696609cfe7568fe3c61790f8b1127a112aa718
Eunice Koh <eunicekoh46@gmail.com>
2024-05-08 14:56:56 +0800

    so cool

--- run/report.py
+++ run/report.py
@@ -3,3 +3,8 @@ from kerykeion import Report, AstrologicalSubject
 eunice = AstrologicalSubject("eunice", 2003, 2, 18, 13, 28, "Singapore")
 report = Report(eunice)
 report.print_report()
+
+eunice.sun
+eunice.first_house
+eunice.moon.element
+
